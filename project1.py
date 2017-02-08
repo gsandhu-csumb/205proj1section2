003406069e87e4f3069e80f4195d194c2d11b2ed
Gurjot Sandhu <gsandhu@csumb.edu>
2017-02-08 20:57:29 +0000

    Message

--- project1.py
+++ project1.py
@@ -1,1 +1,7 @@
-print("hello world")
+resrefrom PIL import Image
+    iimage1 = Image.open('images/1.png')
+    Ipicture1 = image1.load()
+image2 = Image.open('images/2.png')
+    Ipicture2 = image2.load()
+    image2 = Image.open('images/3.png')
+    Ipicture3 = image3.load()
